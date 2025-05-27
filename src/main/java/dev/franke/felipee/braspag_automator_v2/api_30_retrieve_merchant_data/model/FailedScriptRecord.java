74c9605ae076a1cbca6a7db999d769772b9ac8e4
FrankeFelipe5 <frankefelipee@gmail.com>
2025-05-27 12:01:23 -0300

    feat: include record for failed script

---
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/model/FailedScriptRecord.java
@@ -0,0 +1,67 @@
+package dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class FailedScriptRecord {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @JsonProperty("numeroEc")
+  private String ecNumber;
+
+  @JsonProperty("mensagem")
+  private String message;
+
+  @JsonProperty("dataRegistro")
+  @CreationTimestamp
+  private LocalDateTime recordTimestamp;
+
+  public FailedScriptRecord() {}
+
+  public FailedScriptRecord(String ecNumber, String message) {
+    this.ecNumber = ecNumber;
+    this.message = message;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getEcNumber() {
+    return ecNumber;
+  }
+
+  public void setEcNumber(String ecNumber) {
+    this.ecNumber = ecNumber;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public LocalDateTime getRecordTimestamp() {
+    return recordTimestamp;
+  }
+
+  public void setRecordTimestamp(LocalDateTime recordTimestamp) {
+    this.recordTimestamp = recordTimestamp;
+  }
+}
