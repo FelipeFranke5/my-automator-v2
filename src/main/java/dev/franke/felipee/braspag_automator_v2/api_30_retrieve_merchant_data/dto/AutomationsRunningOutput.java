3e8995b00237fa469c1fa55b4a8fd999efc65453
FrankeFelipe5 <frankefelipee@gmail.com>
2025-05-27 12:00:23 -0300

    feat: include pending automations dto

---
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/dto/AutomationsRunningOutput.java
@@ -0,0 +1,7 @@
+package dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record AutomationsRunningOutput(
+    @JsonProperty("automacoesSendoExecutadas") byte numberOfAutomations,
+    @JsonProperty("mensagem") String message) {}
