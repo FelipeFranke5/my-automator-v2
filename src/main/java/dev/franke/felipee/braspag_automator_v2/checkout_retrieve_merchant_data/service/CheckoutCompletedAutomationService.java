a6e6942ca9087653ebeb8c8484735f05456fd77d
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-05 12:03:39 -0300

    feat: include logic to avoid duplicates and include deletion method

--- src/main/java/dev/franke/felipee/braspag_automator_v2/checkout_retrieve_merchant_data/service/CheckoutCompletedAutomationService.java
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/checkout_retrieve_merchant_data/service/CheckoutCompletedAutomationService.java
@@ -5,6 +5,8 @@ import dev.franke.felipee.braspag_automator_v2.checkout_retrieve_merchant_data.d
 import dev.franke.felipee.braspag_automator_v2.checkout_retrieve_merchant_data.model.CheckoutCompletedAutomation;
 import dev.franke.felipee.braspag_automator_v2.checkout_retrieve_merchant_data.repository.CheckoutCompletedAutomationRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,24 +16,44 @@ public class CheckoutCompletedAutomationService {
 
     private static final Logger LOG = LoggerFactory.getLogger(CheckoutCompletedAutomationService.class);
 
-    private final CheckoutCompletedAutomationRepository automationRepository;
+    private final CheckoutCompletedAutomationRepository repository;
 
-    public CheckoutCompletedAutomationService(final CheckoutCompletedAutomationRepository automationRepository) {
-        this.automationRepository = automationRepository;
+    public CheckoutCompletedAutomationService(CheckoutCompletedAutomationRepository repository) {
+        this.repository = repository;
     }
 
-    public void save(final CheckoutCompletedAutomation automation) {
+    private boolean ecIsValid(String ec) {
+        if (ec == null) return false;
+        if (ec.isBlank()) return false;
+        if (ec.length() != 10) return false;
+
+        Optional<CheckoutCompletedAutomation> failOptional = repository.findByEc(ec);
+
+        if (failOptional.isPresent()) return false;
+
         try {
-            LOG.info("Attempting to save result to DB");
-            automationRepository.save(automation);
-        } catch (final Exception exception) {
-            LOG.error("Could not save result to DB", exception);
+            return Long.parseLong(ec) > 0;
+        } catch (NumberFormatException numberFormatException) {
+            return false;
+        }
+    }
+
+    public void save(CheckoutCompletedAutomation automation) {
+        if (ecIsValid(automation.getEc())) {
+            try {
+                LOG.info("Attempting to save result to DB");
+                repository.save(automation);
+            } catch (final Exception exception) {
+                LOG.error("Could not save result to DB", exception);
+            }
+        } else {
+            LOG.warn("Automation is not being saved because EC is not valid ..");
         }
     }
 
     public List<CompletedAutomationOutputForExcel> outputForExcel() {
         LOG.info("Output for Excel called");
-        final List<CheckoutCompletedAutomation> originalList = automationRepository.findAll();
+        final List<CheckoutCompletedAutomation> originalList = repository.findAll();
         return originalList.stream()
                 .map(original -> new CompletedAutomationOutputForExcel(
                         original.getRecordId(),
@@ -53,11 +75,13 @@ public class CheckoutCompletedAutomationService {
 
     public List<CompletedAutomationOutput> outputForJson() {
         LOG.info("Output for Json called");
-        final List<CheckoutCompletedAutomation> originalList = automationRepository.findAll();
+        final List<CheckoutCompletedAutomation> originalList = repository.findAll();
         return originalList.stream()
-                .map(original -> {
-                    return new CompletedAutomationOutput(original.getEc(), original.isBlocked());
-                })
+                .map(original -> new CompletedAutomationOutput(original.getEc(), original.isBlocked()))
                 .toList();
+    }
+
+    public void deleteAll() {
+        repository.deleteAll();
     }
 }
