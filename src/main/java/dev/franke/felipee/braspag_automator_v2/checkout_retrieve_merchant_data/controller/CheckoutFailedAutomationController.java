c88b35e35d82ff6ea6eeaae0d5769d41214cf03e
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-05 12:01:34 -0300

    feat: include controller for failed automation results

---
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/checkout_retrieve_merchant_data/controller/CheckoutFailedAutomationController.java
@@ -0,0 +1,36 @@
+package dev.franke.felipee.braspag_automator_v2.checkout_retrieve_merchant_data.controller;
+
+import dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.controller.HeaderValidator;
+import dev.franke.felipee.braspag_automator_v2.checkout_retrieve_merchant_data.service.CheckoutFailedAutomationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/checkout/retrieve-merchant/failed")
+public class CheckoutFailedAutomationController {
+
+    private final CheckoutFailedAutomationService service;
+    private final HeaderValidator headerValidator;
+
+    public CheckoutFailedAutomationController(CheckoutFailedAutomationService service, HeaderValidator headerValidator) {
+        this.service = service;
+        this.headerValidator = headerValidator;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll(@RequestHeader(name = "Authorization") final String authorizationHeader) {
+        if (!headerValidator.headerIsValid(authorizationHeader)) {
+            return ResponseEntity.status(401).build();
+        }
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAll(@RequestHeader(name = "Authorization") final String authorizationHeader) {
+        if (!headerValidator.headerIsValid(authorizationHeader)) {
+            return ResponseEntity.status(401).build();
+        }
+        service.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
+}
