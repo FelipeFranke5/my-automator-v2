483c773106e6b02613be8baa4d2e5d7b3329ba9f
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-08 01:17:42 -0300

    feat: include custom query to check if an automation was already saved with given EC Number

--- src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/repository/MerchantRepository.java
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/repository/MerchantRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MerchantRepository extends JpaRepository<Merchant, UUID> {}
+public interface MerchantRepository extends JpaRepository<Merchant, UUID> {
+    boolean existsByEc(String ec);
+}
