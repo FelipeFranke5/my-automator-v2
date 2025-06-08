90c57161b56f0f4d05b2ba7fb952d8fa94c5f15e
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-08 01:48:39 -0300

    feat: include custom query to check if EC number is saved

--- src/main/java/dev/franke/felipee/braspag_automator_v2/checkout_retrieve_merchant_data/repository/CheckoutCompletedAutomationRepository.java
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/checkout_retrieve_merchant_data/repository/CheckoutCompletedAutomationRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CheckoutCompletedAutomationRepository extends JpaRepository<CheckoutCompletedAutomation, UUID> {
     Optional<CheckoutCompletedAutomation> findByEc(String ec);
+
+    boolean existsByEc(String ec);
 }
