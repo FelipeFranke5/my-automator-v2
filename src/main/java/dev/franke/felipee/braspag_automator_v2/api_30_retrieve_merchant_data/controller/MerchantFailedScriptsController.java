bd27c93b2aee75886ab7e9fceb20e39c467ea012
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-09 11:00:11 -0300

    refactor: change this class to implement EcSearchMailSender

--- src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/controller/MerchantFailedScriptsController.java
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/controller/MerchantFailedScriptsController.java
@@ -1,14 +1,14 @@
 package dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.controller;
 
-import dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.dto.failed.FailedAutomationOutput;
 import dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.service.FailedScriptService;
+import dev.franke.felipee.braspag_automator_v2.contracts.controller.EcSearchFailureMainController;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/retrieve-merchant/failed")
-public class MerchantFailedScriptsController {
+public class MerchantFailedScriptsController implements EcSearchFailureMainController {
 
     private final FailedScriptService service;
     private final HeaderValidator headerValidator;
@@ -18,8 +18,9 @@ public class MerchantFailedScriptsController {
         this.headerValidator = headerValidator;
     }
 
+    @Override
     @GetMapping
-    public ResponseEntity<List<FailedAutomationOutput>> all(
+    public ResponseEntity<List<?>> getAutomationsWithError(
             @RequestHeader(name = "Authorization") String authorizationHeader) {
         if (!headerValidator.headerIsValid(authorizationHeader)) {
             return ResponseEntity.status(401).build();
@@ -27,8 +28,10 @@ public class MerchantFailedScriptsController {
         return ResponseEntity.ok(service.jsonOutput());
     }
 
+    @Override
     @DeleteMapping
-    public ResponseEntity<Void> deleteAll(@RequestHeader(name = "Authorization") String authorizationHeader) {
+    public ResponseEntity<Void> deleteAutomationsWithError(
+            @RequestHeader(name = "Authorization") String authorizationHeader) {
         if (!headerValidator.headerIsValid(authorizationHeader)) {
             return ResponseEntity.status(401).build();
         }
