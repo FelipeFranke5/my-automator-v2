43370ac36f2c8523039b2051a8afcfbca0f59c85
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-09 10:57:53 -0300

    misc: delete unused body response

--- src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/controller/AutomationBodyResponse.java
+++
@@ -1,5 +0,0 @@
-package dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.controller;
-
-import java.time.LocalDateTime;
-
-public record AutomationBodyResponse(LocalDateTime timestamp, boolean sucess) {}
