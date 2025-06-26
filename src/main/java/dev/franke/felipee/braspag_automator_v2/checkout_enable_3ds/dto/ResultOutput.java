ac5fcb936a81922310791148b27988e6080865f9
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-26 10:43:36 -0300

    refactor: delete enabled 3ds

--- src/main/java/dev/franke/felipee/braspag_automator_v2/checkout_enable_3ds/dto/ResultOutput.java
+++
@@ -1,5 +0,0 @@
-package dev.franke.felipee.braspag_automator_v2.checkout_enable_3ds.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ResultOutput(@JsonProperty("numeroEc") String ec, @JsonProperty("mensagemRetorno") String message) {}
