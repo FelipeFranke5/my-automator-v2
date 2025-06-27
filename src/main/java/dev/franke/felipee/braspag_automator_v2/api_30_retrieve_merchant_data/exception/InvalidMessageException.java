23d20a69a05fa90beb2f51b97c38db19b5c6a5d6
FrankeFelipe5 <frankefelipee@gmail.com>
2025-06-27 11:28:17 -0300

    feat: add new exception

---
+++ src/main/java/dev/franke/felipee/braspag_automator_v2/api_30_retrieve_merchant_data/exception/InvalidMessageException.java
@@ -0,0 +1,7 @@
+package dev.franke.felipee.braspag_automator_v2.api_30_retrieve_merchant_data.exception;
+
+public class InvalidMessageException extends RuntimeException {
+    public InvalidMessageException(String message) {
+        super(message);
+    }
+}
